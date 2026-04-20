0b3ecc4b570cc4bcb5dcf40234f8a0332ab27ae5
Toshimitsu-M <arrinfluedshwman3+git@gmail.com>
2026-04-20 23:37:25 +0900

    fix: カスタムドメイン用にbaseを/に変更

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/frontend-vue/' : './',
+  base: '/',
   plugins: [
     vue(),
     tailwindcss(),
